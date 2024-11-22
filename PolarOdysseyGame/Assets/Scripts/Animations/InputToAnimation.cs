dacb396c0f48121a5a63d10e2b5b2c5df014383b
Mihael Lešković <mihael.leskovic@fer.hr>
2024-11-22 23:47:16 +0100

    walking to the side animation first look

---
+++ PolarOdysseyGame/Assets/Scripts/Animations/InputToAnimation.cs
@@ -0,0 +1,71 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InputToAnimation : MonoBehaviour
+{
+    public Animator animator;
+    public float minimumSpeedToWalk = 0.1f;
+    public float maximumSpeedToStandStill = 0.9f;
+
+    public float xInput;
+    public float yInput;
+    public bool isWalking;
+
+    private IState state;
+    
+    void Start()
+    {
+        animator = GetComponent<Animator>();
+        state = new IdleState(this);
+    }
+
+    void Update()
+    {
+        xInput = Input.GetAxis("Horizontal");
+        yInput = Input.GetAxis("Vertical");
+
+        state.CalculateWalkingState();
+
+        animator.SetFloat("xMovement", xInput);
+        animator.SetFloat("yMovement", yInput);
+        animator.SetBool("isWalking", isWalking);
+    }
+
+    bool isAboveMinimum() {
+        return Math.Abs(xInput) > minimumSpeedToWalk || Math.Abs(yInput) > minimumSpeedToWalk;
+    }
+
+    bool isUnderMaximum() {
+        return Math.Abs(xInput) < maximumSpeedToStandStill && Math.Abs(yInput) < maximumSpeedToStandStill;
+    }
+
+    abstract class IState {
+        protected InputToAnimation parent;
+        public IState(InputToAnimation parent) {
+            this.parent = parent;
+        }
+        public virtual void CalculateWalkingState(){}
+    }
+
+    class IdleState : IState {
+        public IdleState(InputToAnimation parent) : base(parent){}
+
+        override public void CalculateWalkingState() {
+            parent.isWalking = parent.isAboveMinimum();
+
+            if(!parent.isUnderMaximum()) parent.state = new WalkingState(parent);
+        }
+    }
+
+    class WalkingState : IState {
+        public WalkingState(InputToAnimation parent) : base(parent){}
+
+        override public void CalculateWalkingState() {
+            parent.isWalking = !parent.isUnderMaximum();
+
+            if(!parent.isAboveMinimum()) parent.state = new IdleState(parent);
+        }
+    }
+}
