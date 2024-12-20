8b1f588231b3247e4df888058e406e239f69215e
Mihael Lešković <mihael.leskovic@fer.hr>
2024-12-20 20:41:51 +0100

    changes
    refactoring
    interactor changes - ray->collider picking

--- PolarOdysseyGame/Assets/Scripts/Interactable/Interactor.cs
+++ PolarOdysseyGame/Assets/Scripts/Interactable/Interactor.cs
@@ -5,33 +5,79 @@ using UnityEngine;
 
 interface IInteractable
 {
-    void Interact();
     void Interact(Interactor interactor);
 }
 
+
+/*
+this script is meant to be put on the interactor collider
+*/
 public class Interactor : MonoBehaviour
 {
-    public Transform interactorSource;
+    public Transform interactorPosition;
     public float interactRange = 3.5f;
-    private bool isInteracting = false;
 
+    private bool isInteracting = false;
+    private GameObject lastInteracted;
+
+    private GameObject player;
+    private List<GameObject> interactables = new List<GameObject>();
+
+/*
+interactor may walk around; once it approaches an interactable, detected by a collision with a trigger
+    upon entering the trigger, the interactable is added to the list of possible interactables;
+        -> we need a way to choose from this list; 
+        probably best to either:
+            a) take the angle between the player's front vector and the interactable's position
+            b) take the distance from the player's position to interactable's position
+            c) take the multiple of a) and b) to make it possible to reach things player is looking at, but are farther away
+        option b) will be taken for its simplicity and then changed if it proves inadequate
+*/
     void Start()
     {
-        if(interactorSource == null) interactorSource = this.transform;
+        if(interactorPosition == null) interactorPosition = this.transform;
+        player = GameObject.FindGameObjectWithTag("Player");
     }
 
     void Update()
     {
-        if(Input.GetKeyDown(KeyCode.E) && !isInteracting) FindAndInteract();
+        ProcessInput();
+    }
+
+    void ProcessInput(){
+        if(!Input.GetButtonDown("Interact")) return;
+
+        if(!isInteracting) FindAndInteract();
+        else lastInteracted.GetComponent<IInteractable>().Interact(this);
+    }
+
+    void OnTriggerExit(Collider other) {
+        if(other.gameObject.GetComponent<IInteractable>() == null) return;
+        interactables.Remove(other.gameObject);
+    }
+
+    void OnTriggerEnter(Collider other){
+        if(other.gameObject.GetComponent<IInteractable>() == null) return;
+        interactables.Add(other.gameObject);
     }
 
     void FindAndInteract()
     {
-        if(Physics.Raycast(interactorSource.position, interactorSource.forward, out RaycastHit hit, interactRange))
-        {
-            Debug.Log("Interacting with " + hit.transform.name);
-            if(hit.collider.gameObject.TryGetComponent(out IInteractable interactable)) interactable.Interact(this);
+        if(interactables.Count == 0) return;
+        float minDistance = float.MaxValue;
+        GameObject closestGo = null;
+
+        float distance;
+        foreach(GameObject go in interactables) {
+            distance = (go.transform.position - player.transform.position).magnitude;
+            if(minDistance > distance) {
+                minDistance = distance;
+                closestGo = go;
+            }
         }
+
+        lastInteracted = closestGo;
+        closestGo.GetComponent<IInteractable>().Interact(this);
     }
 
     public void SetInteracting(bool interacting) {
