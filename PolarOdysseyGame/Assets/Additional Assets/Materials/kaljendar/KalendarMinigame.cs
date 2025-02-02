ace8dbbe2b7e5317e7ae8148d597b664deaf94d9
Mihael Lešković <mihael.leskovic@fer.hr>
2025-02-02 17:03:09 +0100

    changes

---
+++ PolarOdysseyGame/Assets/Additional Assets/Materials/kaljendar/KalendarMinigame.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class KalendarMinigame : MonoBehaviour, IMinigameLogicController
+{
+    public MinigameInteractable minigameInteractable;
+
+
+    void Start(){
+        Canvas canvas = this.gameObject.GetComponent<Canvas>();
+        Camera mainCamera = Camera.main;
+        if(canvas == null || mainCamera == null) {
+            Debug.LogWarning("Canvas or MainCamera not found!");
+            return;
+        }
+        canvas.worldCamera = mainCamera;
+    }
+
+    public void setMinigameInteractable(MinigameInteractable val)
+    {
+        this.minigameInteractable = val;
+    }
+}
