b301eb583dc7bbf22c9946a700ea081091a7c7cc
tp52607 <117480302+tp52607@users.noreply.github.com>
2025-02-02 15:58:41 +0100

    gotov minigame, nadam se funkcionalan

--- PolarOdysseyGame/Assets/Additional Assets/Materials/kaljendar/DragAndDrop.cs
+++ PolarOdysseyGame/Assets/Additional Assets/Materials/kaljendar/DragAndDrop.cs
@@ -16,6 +16,7 @@ public class DragAndDrop : MonoBehaviour, IBeginDragHandler, IDragHandler, IEndD
 
     public void OnBeginDrag(PointerEventData eventData)
     {
+        Debug.Log("Begin drag");
         startPosition = rectTransform.position;
         startParent = transform.parent;
         canvasGroup.blocksRaycasts = false;
@@ -23,11 +24,13 @@ public class DragAndDrop : MonoBehaviour, IBeginDragHandler, IDragHandler, IEndD
 
     public void OnDrag(PointerEventData eventData)
     {
+        Debug.Log("dragging");
         rectTransform.position = eventData.position;
     }
 
     public void OnEndDrag(PointerEventData eventData)
     {
+        Debug.Log("end drag");
         canvasGroup.blocksRaycasts = true;
 
         if (transform.parent == startParent) // Ako nije postavljen na slot, vrati na početnu poziciju
